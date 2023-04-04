3e77b824a8982185169f558c284de7415c1e72e4
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-04-03 21:34:25 -0400

    New features added but there's an error

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         OlderCode add=new OlderCode();
         add.name="Older codes here";
+
+        SezarCode add2=new SezarCode();
+        add2.name="Sezar";
+        add2.age=34;
     }
 }
