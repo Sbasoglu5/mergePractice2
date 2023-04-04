9c6b45188b4a7c1730d4d18bb192dace278c2ec9
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-04-03 21:30:36 -0400

    Katie's code

---
+++ src/KatieCode.java
@@ -0,0 +1,3 @@
+public class KatieCode {
+    String name;
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         OlderCode add=new OlderCode();
         add.name="Older codes here";
+
+        KatieCode add1=new KatieCode();
+        add1.name="Katie";
     }
 }
