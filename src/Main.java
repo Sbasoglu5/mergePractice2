d5da6a7478e2faaee5edba14289a3d5c19776760
Sbasoglu5 <sezarbasoglu@gmail.com>
2023-04-03 21:25:58 -0400

    old codes

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        OlderCode add=new OlderCode();
+        add.name="Older codes here";
+    }
+}
---
+++ src/OlderCode.java
@@ -0,0 +1,4 @@
+public class OlderCode {
+    String name;
+
+}
